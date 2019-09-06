0b2d79773d7ce08b9b1986dc9b0634e3b1436fad
Josue <jderazo4@espe.edu.ec>
2019-09-06 13:47:17 -0500

    Se integro pug como dependencia de desarrollo, se actulizo el readme, se fijo como predeterminado el fixedheader y el logo y se quito el inicio automático en el navegador.

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'vue Admin Template' // page title
+const name = defaultSettings.title || 'Admin Evolution School' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
@@ -31,7 +31,7 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
-    open: true,
+    open: false,
     overlay: {
       warnings: false,
       errors: true
